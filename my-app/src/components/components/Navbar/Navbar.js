63df7a982ef4235f1ca3cbab280a3225344f9344
Mike Matheny <mike.matheny.81@gmail.com>
2019-08-09 00:31:57 -0400

    working on navbar

--- my-app/src/components/components/Navbar/Navbar.js
+++ my-app/src/components/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Collapse, Navbar, NavbarBrand, NavbarToggler, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, Row, Col, Container } from 'reactstrap';
 import './Navbar.css'
 
 class NavExample extends React.Component {
@@ -19,39 +19,54 @@ class NavExample extends React.Component {
   }
   render() {
     return (
-      <div>
-        <Navbar expand="md">
-          <NavbarBrand href="/">419-668-0000</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">Auctioneer Services</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Items for Sale
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Lamps
-                  </DropdownItem>
-                  <DropdownItem>
-                    Furniture
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Miscellaneous
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+      <div className="app">
+        <Container fluid>
+              <Navbar expand="md">
+                <Row>
+
+                  <NavbarToggler onClick={this.toggle} />
+                  <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <Col xs="10">
+                    <NavItem>
+                      <NavLink href="/">419-668-0000</NavLink>
+                    </NavItem>
+                  </Col>
+                    <Col xs="10">
+                        <NavItem>
+                          <NavLink href="/components/">Home</NavLink>
+                        </NavItem>
+                    </Col>
+                    <Col xs="10">
+                        <NavItem>
+                          <NavLink href="https://github.com/reactstrap/reactstrap">Auctioneer Services</NavLink>
+                        </NavItem>
+                    </Col>
+                    <Col xs="10">
+                        <UncontrolledDropdown nav inNavbar>
+                          <DropdownToggle nav caret>
+                            Items for Sale
+                          </DropdownToggle>
+                          <DropdownMenu right>
+                            <DropdownItem>
+                              Lamps
+                            </DropdownItem>
+                            <DropdownItem>
+                              Furniture
+                            </DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem>
+                              Miscellaneous
+                            </DropdownItem>
+                          </DropdownMenu>
+                        </UncontrolledDropdown>
+                  </Col>
+                  </Nav>
+                  </Collapse>
+                </Row>
+          </Navbar>
+      </Container>
+    </div>
     );
   }
 }
