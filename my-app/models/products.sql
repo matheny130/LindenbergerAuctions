5aa92ab65166a2c8d9ee38eb2dce3b44dbae2819
Mike Matheny <mike.matheny.81@gmail.com>
2019-08-18 10:15:41 -0400

    working on db

--- my-app/models/products.sql
+++ my-app/models/products.sql
@@ -23,3 +23,16 @@ ALTER TABLE products MODIFY description varchar (500) NOT NULL;
 ALTER TABLE products MODIFY picture blob NOT NULL;
 
 DESCRIBE products;
+
+ALTER TABLE products MODIFY id INT AUTO_INCREMENT;
+
+INSERT INTO products(category, title, price, description, picture)
+VALUES ("lamp", "gas lamp", 50.00, "Vintage gas lamp. Beautifully restored.", "lamp1.jpg"), 
+("furniture", "rolltop desk", 2000.00, "Antique Rolltop Desk. No scratches or dings. All drawers open.", "desk1.jpg"),
+("miscellaneous", "antique telephone", 300.00, "Antique Telephone. No longer operationational.", "phone1.jpg");
+
+DESCRIBE products;
+
+SHOW tables;
+
+SELECT * from products;
